b994953c3d9e09ece44741c11f15d1b474d6a877
Chi Yin Li <chiyinli.mastercyber1@gmail.com>
2020-04-21 16:07:44 +0100

    first ios swift lesson

---
+++ chiyinli/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  chiyinli
+//
+//  Created by Chi Yin Li on 20/04/2020.
+//  Copyright © 2020 Chi Yin Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var countdown: UILabel!
+    var timer = Timer()
+    var seconds = 3
+    var segues = ["goToBigHero","goToToyStory","goToTangled","goToRatatouille"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countdown.text = "Ready..."
+        
+    }
+
+    @IBAction func whichMovie(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerCount), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerCount(){
+        if (seconds > 0){
+            countdown.text = String(seconds)
+            seconds -= 1
+        } else {
+            timer.invalidate()
+            countdown.text = "Ready..."
+            seconds = 3
+            let randomNumber = Int.random(in: 0...3)
+            self.performSegue(withIdentifier: segues[randomNumber], sender: self)
+            
+        }
+
+    }
+}
---
+++ chiyinliTests/chiyinliTests.swift
@@ -0,0 +1,34 @@
+//
+//  chiyinliTests.swift
+//  chiyinliTests
+//
+//  Created by Chi Yin Lee on 20/04/2020.
+//  Copyright © 2020 Chi Yin Lee. All rights reserved.
+//
+
+import XCTest
+@testable import chiyinli
+
+class chiyinliTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
